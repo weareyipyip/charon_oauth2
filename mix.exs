fdea4a76748a59ee66f9256ec4d9743e8fe9baf4
Julien Smeets <16883019+juulSme@users.noreply.github.com>
2022-07-04 15:46:03 +0200

    VS code, Github actions, dev config

--- mix.exs
+++ mix.exs
@@ -4,10 +4,11 @@ defmodule CharonOauth2.MixProject do
   def project do
     [
       app: :charon_oauth2,
-      version: "0.1.0",
+      version: "0.0.0+development",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
 
@@ -21,8 +22,11 @@ defmodule CharonOauth2.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.21", only: [:dev, :test], runtime: false},
+      {:charon, git: "git@github.com:weareyipyip/charon.git"}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
