9c5fc9487ce50c3ae473df14e0299cd0206b64ab
Julien Smeets <16883019+juulSme@users.noreply.github.com>
2022-07-04 15:34:57 +0200

    Empty Elixir project

---
+++ lib/charon_oauth2.ex
@@ -0,0 +1,18 @@
+defmodule CharonOauth2 do
+  @moduledoc """
+  Documentation for `CharonOauth2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CharonOauth2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/charon_oauth2_test.exs
@@ -0,0 +1,8 @@
+defmodule CharonOauth2Test do
+  use ExUnit.Case
+  doctest CharonOauth2
+
+  test "greets the world" do
+    assert CharonOauth2.hello() == :world
+  end
+end
